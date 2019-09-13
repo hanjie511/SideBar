711dda58a183e7753972cdfdf18d01ca18f63d28
hanjie511 <hanjie511@gmail.com>
2019-09-13 23:04:22 +0800

    RecyclerView列表的数据适配器

---
+++ app/src/main/java/com/example/hj/siderbar/MyAdapter.java
@@ -0,0 +1,61 @@
+package com.example.hj.siderbar;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.VH> {
+    ArrayList<String> list;
+    public static Map<String,Integer> index_map=new HashMap<String, Integer>();
+    public static class VH extends RecyclerView.ViewHolder{
+        TextView groupText;
+        TextView username;
+        LinearLayout linear;
+
+        public VH(View view){
+            super(view);
+            groupText=view.findViewById(R.id.group_text_item);
+            username=view.findViewById(R.id.username_item);
+            linear=view.findViewById(R.id.username_linear);
+        }
+    }
+    public MyAdapter(ArrayList<String> list){
+        this.list=list;
+    }
+
+    @NonNull
+    @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item,viewGroup,false);
+        VH vh=new VH(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VH vh, int i) {
+        String name=list.get(i);
+        if(name.length()==1){
+            index_map.put(name,i);
+            vh.linear.setVisibility(View.GONE);
+            vh.groupText.setVisibility(View.VISIBLE);
+            vh.groupText.setText(name);
+       }else{
+            vh.linear.setVisibility(View.VISIBLE);
+            vh.groupText.setVisibility(View.GONE);
+            vh.username.setText(name);
+       }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
