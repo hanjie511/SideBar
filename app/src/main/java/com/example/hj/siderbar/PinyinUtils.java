7a5e37130779acb47b20cf04521fc9c100b28f74
hanjie511 <hanjie511@gmail.com>
2020-02-03 15:06:18 +0800

    排序算法优化

--- app/src/main/java/com/example/hj/siderbar/PinyinUtils.java
+++ app/src/main/java/com/example/hj/siderbar/PinyinUtils.java
@@ -7,12 +7,15 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 
 public class PinyinUtils {
-    private static  String [] alpha_array={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    private static String[] alpha_array = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
     /*
     该方法将字符串的首个汉字字符转化为拼音
      */
@@ -38,31 +41,38 @@ public class PinyinUtils {
     /*
     将传过来的字符串列表按A-Z的顺序排序后返回
      */
-    public  static ArrayList<String> getSortedListByAlpha(LinkedList<String> list) {
-        ArrayList<String> total_list = new ArrayList< String>();
-        String first_alpha="";
-        boolean flag=true;
-        String str="";
-        for (int i=0;i<alpha_array.length;i++) {//将字母按A-Z的顺序依次遍历排序
-            flag=true;
-            for (int j=0;j<list.size();j++) {
-                str=list.get(j);
-                first_alpha  = PinyinUtils.toHanyuPinyin(str);
-                if(alpha_array[i].equals(first_alpha)){
-                    if(flag){
-                        total_list.add(alpha_array[i]);
-                        flag=false;
+    public static ArrayList<String> getSortedListByAlpha(LinkedList<String> list) {
+        long time1 = System.currentTimeMillis();
+        ArrayList<String> total_list = new ArrayList<String>();
+        String first_alpha = "";
+        boolean flag = true;
+        String str = "";
+        for (int i = 0; i < alpha_array.length; i++) {//将字母按A-Z的顺序依次遍历排序
+            flag = true;
+            Iterator<String> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                str = iterator.next();
+                first_alpha = "" + PinyinUtils.toHanyuPinyin(str).charAt(0);
+                if (alpha_array[i].equals(first_alpha)) {
+                    if (flag) {
+                        total_list.add(alpha_array[i].toUpperCase());
+                        flag = false;
                     }
                     total_list.add(str);
-                    list.remove(j);
+                    iterator.remove();
                 }
             }
         }
-        if(list.size()>0){//将首字母不是A-Z的数组放在最后
+        if (list.size() > 0) {//将首字母不是A-Z的数组放在最后
             total_list.add("#");
-            for(String s:list){
+            for (String s : list) {
                 total_list.add(s);
             }
+        }
+        long time2 = System.currentTimeMillis();
+        System.out.println("time:" + (time2-time1));
+        for (String s : total_list) {
+            System.out.println("alpha:" + s);
         }
         return total_list;
     }
